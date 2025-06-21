f1e312fd23ffe8294cce57deb486d31667fb7d3b
Nexuss-void <channicholas8@gmail.com>
2025-06-21 18:50:31 +0700

    update pos2

--- pos/pos/settings.py
+++ pos/pos/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kti41m+yqi0^1s6f5c%gm!&ixm9x!i+x(ogelj_gx-d&c&82k7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS = []
