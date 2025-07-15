98b1395d89064f71fe77ddea8316ba629e8c397d
Nexuss-void <channicholas8@gmail.com>
2025-07-15 15:17:33 +0700

    update database 5

--- pos/pos/settings.py
+++ pos/pos/settings.py
@@ -93,8 +93,12 @@ DATABASES = {
 
     # 'default': dj_database_url.config(default='sqlite:///db.sqlite3', conn_max_age=600),
 
-    'default': dj_database_url.config(default=os.environ.get('mysql://root:anHiwjARgVsecmSFhovOEYgzFozbncrd@mysql.railway.internal:3306/railway'))
+    # 'default': dj_database_url.config(default=os.environ.get('mysql://root:anHiwjARgVsecmSFhovOEYgzFozbncrd@mysql.railway.internal:3306/railway'))
 
+    'default': dj_database_url.config(
+        default=os.environ.get('mysql://root:anHiwjARgVsecmSFhovOEYgzFozbncrd@mysql.railway.internal:3306/railway'),
+        engine='django.db.backends.mysql'
+    )
 }
 
 
