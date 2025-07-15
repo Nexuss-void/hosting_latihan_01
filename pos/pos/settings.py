ee0364e0ffeff7ef170b959e400e91cb95efc7ca
Nexuss-void <channicholas8@gmail.com>
2025-07-15 15:04:22 +0700

    update database 4

--- pos/pos/settings.py
+++ pos/pos/settings.py
@@ -90,7 +90,11 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3', conn_max_age=600),
+
+    # 'default': dj_database_url.config(default='sqlite:///db.sqlite3', conn_max_age=600),
+
+    'default': dj_database_url.config(default=os.environ.get('mysql://root:anHiwjARgVsecmSFhovOEYgzFozbncrd@mysql.railway.internal:3306/railway'))
+
 }
 
 
